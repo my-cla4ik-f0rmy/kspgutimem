a6cec35c710c07edbf017286d1cb3a14e10a07b5
my-cla4ik-f0rmy <retiree-frets-0r@icloud.com>
2022-08-29 01:43:56 +0400

    Обнова не?
    Добавил генерацию аваторок для учебных чатов.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,9 @@
-
+var   img1, img2;
 let font,
   fontsize = 80;
   fontsize2 = 48;
+  fontsize3 = 180;
+
 
 
 
@@ -14,20 +16,9 @@ function preload() {
   // is loaded before setup() and draw() are called
   font = loadFont('font/OpenSans-Bold.ttf');
   font2 = loadFont('font/OpenSans-SemiBold.ttf');
-  
+  img1 = loadImage('images/student.png')
 }
-
-
-
-
-
-
 function setup() {
-  
-
- vm =1
-  
-  if (vm == 1){
     var b = document.getElementById('sketch-div');
     var w = b.clientWidth;
     var h = b.clientHeight;
@@ -35,63 +26,57 @@ function setup() {
     height = h
     let canvas = createCanvas(width, height);
     canvas.parent('sketch-div');
-    pg = createGraphics(1080, 1080);
-    pg.textAlign(LEFT, TOP);
-  } else{
-    var b = document.getElementById('sketch-div');
-    var w = b.clientWidth;
-    var h = b.clientHeight;
-    width = w
-    height = h
-    let canvas = createCanvas(w, h);
-    canvas.parent('sketch-div');
-    pg = createGraphics(1080, 1080);
-    pg.textAlign(RIGHT, TOP);
-  }
-  
-  
-  
+    pg = createGraphics(1080, 1080);}
 
+
+
+
+function draw() {
+  type = document.querySelector('input[name="type1"]:checked').value;
+  if(type == '1'){
+    post_sm();
+    image(pg, 0, 0, width, height);
+
+  }else if(type == '2'){
+    ava_chat(); 
+    image(pg, 0, 0, width, height); 
+
+  }
   
 
 }
-
-function draw() {
-  assa = document.querySelector('input[name="options"]:checked').value;
-  
- 
+function post_sm(){
+  color_rec = document.querySelector('input[name="options"]:checked').value;
   const name = document.getElementById("sample5").value;
   const name2 = document.getElementById("sample6").value;
-  const textno = name + name2
   background('#345534')
   pg.background('#fdssdf')
   pg.noStroke();
-  pg.fill(assa)
+  pg.fill(color_rec)
   pg.rect(810, 0, 270, 1080)
   pg.fill('#000000')
   pg.textFont(font);
   pg.textSize(fontsize)
   pg.textWrap(WORD);
+  pg.textAlign(LEFT, TOP);
   pg.text(name, 50, 50, 707);
   pg.textFont(font2);
   pg.textSize(fontsize2)
-  
-  if (textWidth(name) > 90){
-    var tet = name.length
-    pg.text(name2, 50, 300, 707, 664);
-  } else{
-    pg.text(name2, 50, 200, 707, 664);
-  }
-  
-  //pg.save("pg.png"); 
-  //save("MyImage1")
-
   image(pg, 0, 0, width, height);
-  
 }
 
+function ava_chat(){
+  const name = document.getElementById("sample5").value;
+  pg.image(img1, 0,0,1080,1080)
+  pg.textFont(font);
+  pg.textSize(fontsize3)
+  pg.textWrap(WORD);
+  pg.textLeading(180);
+  pg.textAlign(CENTER, CENTER);
+  pg.text(name, 540, 500, 0);
+  image(pg, 0, 0, width, height);
 
-
+}
 function save(){
   pg.save("йцуйцу"); 
 }
